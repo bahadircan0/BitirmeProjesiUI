2e2de7690ef0bb281bb7a4af241fe985cf659f06
Bahadır Can <bahadir.can@ideateknoloji.com.tr>
2026-05-27 16:53:43 +0300

    Login ekranı yorum satırları kaldırılmıştır.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,27 +3,27 @@ import { useNavigate, Link } from "react-router-dom";
 import axios from "axios";
 import { jwtDecode } from "jwt-decode"; 
 function Login() {
-  // Formdaki verileri tutacağımız state'ler (hafızalar)
+  
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
   
-  // Sayfa değiştirmek için kullanacağımız hook
+ 
   const navigate = useNavigate();
 
-  // Giriş Yap butonuna basılınca çalışacak fonksiyon
+ 
   const handleLogin = async (e) => {
-    e.preventDefault(); // Sayfanın yenilenmesini engeller
-    setError(""); // Eski hataları temizler
+    e.preventDefault(); 
+    setError(""); 
 
     try {
-      // 1. Backend'e isteği atıyoruz (Kendi Swagger portunu buraya yaz!)
+      
       const response = await axios.post("https://localhost:7080/api/Auth/login", {
         email: email,
         password: password,
       });
 
-      // 2. İstek başarılıysa Token'ı al ve tarayıcının hafızasına kaydet
+    
       const token = response.data.token;
       const decoded = jwtDecode(token);
         const userRole = decoded["http://schemas.microsoft.com/ws/2008/06/identity/claims/role"];
@@ -32,12 +32,11 @@ function Login() {
 
 
       
-      // Şimdilik başarılı olunca bir yere gitmesine gerek yok, ama ileride toplantı sayfasına yönlendireceğiz
-      // navigate("/dashboard"); 
+     
       navigate("/dashboard");
 
     } catch (err) {
-      // 3. Hata varsa ekrana yazdır (Backend'den dönen BadRequest mesajını yakalar)
+      
       setError(err.response?.data || "Giriş yapılamadı. E-posta veya şifre hatalı.");
     }
   };
@@ -47,7 +46,7 @@ function Login() {
       <div style={{ padding: "30px", border: "1px solid #ccc", borderRadius: "8px", width: "300px" }}>
         <h2>Giriş Yap 🔐</h2>
         
-        {/* Hata mesajı varsa kırmızı renkte göster */}
+        
         {error && <p style={{ color: "red", fontWeight: "bold" }}>{error}</p>}
 
         <form onSubmit={handleLogin}>
